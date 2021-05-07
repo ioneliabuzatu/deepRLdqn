4991272688af1950da5772b68da20b309c5866b6
ioneliabuzatu <buzatuionelia@gmail.com>
2021-05-07 12:40:52 +0200

    ass2

---
+++ 04_PongPongPong_Exercise.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import random
+from collections import deque, namedtuple
+
+import numpy as np
+import onnxruntime
+import torch
+import torch.nn as nn
+import torch.onnx
+import torch.optim as optim
+
+from utils import make_atari
+from utils import plot
+from utils import set_seed
+from utils import wrap_deepmind
+from utils import wrap_pytorch
+from utils import to_numpy
+from tqdm import tqdm
+import wandb
+import config
+
+Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
+
+
+# Replay buffer- stores a transitions - state, action, reward, next_state
+# DQN network class - neural network - CNN -> linear layers
+#                   - Add forward should give for a state and action pair, the expected return from t to T
+# Fill_buffer function - add n samples to the replay buffer
+# Train function - sample transitions and update network
+
+
+# TODO: Implement Prioritized Replay Buffer
+# Code Block: (a)
+class ReplayBuffer():
+    def __init__(self, num_actions, memory_len=10000):
+        self.memory_len = memory_len
+        self.transition = []
+        self.num_actions = num_actions
+
+    def add(self, state, action, reward, next_state, done):
+        if self.length() > self.memory_len:
+            self.remove()
+        self.transition.append(Transition(state, action, reward, next_state, done))
+
+    def sample_batch(self, batch_size=32):
+        minibatch = random.sample(self.transition, batch_size)
+
+        states_mb, a_, reward_mb, next_states_mb, done_mb = map(np.array, zip(*minibatch))
+
+        mb_reward = torch.from_numpy(reward_mb).cuda()
+        mb_done = torch.from_numpy(done_mb.astype(int)).cuda()
+
+        a_mb = np.zeros((a_.size, self.num_actions))
+        a_mb[np.arange(a_.size), a_] = 1
+        mb_a = torch.from_numpy(a_mb).cuda()
+
+        return states_mb, mb_a, mb_reward, next_states_mb, mb_done
+
+    def length(self):
+        return len(self.transition)
+
+    def remove(self):
+        self.transition.pop(0)
+
+
+class DQNNetwork_atari(nn.Module):
+
+    def __init__(self, num_actions=4, features_maps=32, activation=nn.ReLU()):
+        super().__init__()
+
+        self.num_actions = num_actions
+        self.activation = activation
+
+        self.cnn_layer_1 = nn.Conv2d(4, features_maps, 8, stride=4)
+        self.cnn_layer_2 = nn.Conv2d(features_maps, features_maps * 2, 4, stride=2)
+        self.cnn_layer_3 = nn.Conv2d(features_maps * 2, features_maps * 2, 3, stride=1)
+
+        feature_map_for_linear_layer = calculate_output_features(
+            [self.cnn_layer_1, self.cnn_layer_2, self.cnn_layer_3], 84
+        )
+
+        # self.fc_1 = nn.Linear(feature_map_for_linear_layer, 600)
+        self.fc_1 = nn.Linear(3136, 512)
+        self.fc_2 = nn.Linear(512, self.num_actions)
+
+    def forward(self, x):
+        # output forward should always be q values for all actions
+        x = torch.tensor(x, dtype=torch.float).cuda()
+        if len(x.size()) == 3:
+            x = x.unsqueeze(dim=0)
+        x = self.cnn_layer_1(x)
+        x = self.activation(x)
+        x = self.cnn_layer_2(x)
+        x = self.activation(x)
+        x = self.cnn_layer_3(x)
+        x = self.activation(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_1(x)
+        x = self.activation(x)
+        x = self.fc_2(x)
+
+        return x
+
+
+def calculate_output_features(layers: list, input_width: int):
+    """ Initially the feature_map_size is the width of the input """
+    for layer in layers:
+        padding = layer.padding[0]
+        stride = layer.stride[0]
+        kernel = layer.kernel_size[0]
+        feature_map_size = (input_width - kernel + 2 * padding) / stride + 1
+    return int(feature_map_size)
+
+
+def soft_update(local_model, target_model, tau):
+    """Soft update model parameters.
+    θ_target = τ*θ_local + (1 - τ)*θ_target
+    """
+    for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+        target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+
+def main():
+    checkpoint_path = "checkpoint"
+
+    wandb.run = config.tensorboard.run
+
+    num_episodes = 300  # number of episodes to run the algorithm
+    buffer_size = 10 ** 4 * 3  # size of the buffer to use
+    epsilon = 1.0  # initial probablity of selecting random action a, annealed over time
+    minibatch_size = 128  # size of the minibatch sampled
+    gamma = 0.99  # discount factor
+    eval_episode = 100
+    num_eval = 10
+    tau = 1e-3  # hyperparameter for updating the target network
+    learning_rate = 0.0001
+    update_after = 200  # update after num time steps
+    epsilon_decay = 10 ** 5
+    epsilon_ub = 1.0
+    epsilon_lb = 0.02
+
+    # DO NOT MAKE CHANGES HERE
+    # Initialize environment
+    # Code Block: (c)
+    env_id = "PongNoFrameskip-v4"
+    env = make_atari(env_id)
+    env = wrap_deepmind(env)
+    env = wrap_pytorch(env)
+    num_actions = env.action_space.n
+    state_space = env.observation_space.shape
+
+    set_seed(env, 0)
+
+    dqn = DQNNetwork_atari(num_actions=num_actions).cuda()
+    dqn_target = DQNNetwork_atari(num_actions=num_actions).cuda()
+
+    # try:
+    #     load_checkpoint = torch.load(checkpoint_path)
+    #     dqn.load_state_dict(load_checkpoint["dqn_state_model"])
+    #     dqn_target.load_state_dict(load_checkpoint["dqn_target_state_model"])
+    #     epsilon_ub = load_checkpoint["epsilon"]
+    #     buffer = load_checkpoint["buffer"]
+    # except FileNotFoundError:
+    #     print("No checkpoint found")
+
+    # Train the agent using DQN for Pong
+    returns = []
+    returns_50 = deque(maxlen=50)
+    losses = []
+    buffer = ReplayBuffer(num_actions=num_actions, memory_len=buffer_size)
+    optimizer = optim.Adam(dqn.parameters(), lr=learning_rate)
+    mse = torch.nn.MSELoss()
+
+    state = env.reset()
+    timesteps = 0
+    dones = 0
+    for i in tqdm(range(num_episodes)):
+        ret = 0
+        done = False
+        while not done:
+            # Decay epsilon :
+            epsilon = max(epsilon_lb, epsilon_ub - timesteps / epsilon_decay)
+            # action selection
+            if np.random.choice([0, 1], p=[1 - epsilon, epsilon]) == 1:
+                a = np.random.randint(low=0, high=num_actions, size=1)[0]
+            else:
+                net_out = dqn(state).detach().cpu().numpy()
+                a = np.argmax(net_out)
+            next_state, r, done, info = env.step(a)
+            ret = ret + r
+
+            buffer.add(state, a, r, next_state, done)
+
+            state = next_state
+            timesteps = timesteps + 1
+
+            # update policy using temporal difference
+            if buffer.length() > minibatch_size and buffer.length() > update_after:
+                optimizer.zero_grad()
+
+                states_mb, mb_a, mb_reward, next_states_mb, mb_done = buffer.sample_batch()
+
+                next_q = torch.max(dqn_target(next_states_mb), 1)[0]
+                targets = mb_reward + (gamma * next_q * (1 - mb_done))
+
+                predictions = dqn(states_mb)
+
+                # Update loss: mse = mean squared error
+                loss = mse(predictions.float(), targets.unsqueeze(-1).float())
+                loss.backward(retain_graph=False)
+                optimizer.step()
+                losses.append(loss.item())
+                wandb.log({"loss": loss.item()}, step=timesteps)
+
+                soft_update(dqn, dqn_target, tau)
+            if done:
+                state = env.reset()
+                print(f"Episode: {i} {ret} epsilon={epsilon}")
+                wandb.log({"Reward": ret, "step_reward": i, "epsilon": epsilon})
+                dones += 1
+                break
+        returns.append(ret)
+        returns_50.append(ret)
+        if i % 20 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i, np.mean(returns_50)))
+        if i % 20 == 0:
+            if not os.path.exists("./checkpoints/"):
+                os.makedirs("./checkpoints/")
+            torch.save({
+                "dqn_state_model": dqn.state_dict(),
+                "dqn_target_state_model": dqn_target.state_dict(),
+                "epsilon": epsilon,
+                "buffer": buffer,
+            },
+                f"checkpoint")
+
+    plot(timesteps, returns, losses)
+
+    state = env.reset()
+
+    torch.onnx.export(dqn,  # model
+                      torch.tensor(state, dtype=torch.float),  # example model input
+                      'submission_onnx.onnx',  # name of the submission
+                      export_params=True,  # save trained parameters
+                      opset_version=10,
+                      do_constant_folding=True)
+
+    # test your onnx model using runtime - we are going to use this to test your model
+    # on the server.
+
+    ort_session = onnxruntime.InferenceSession("submission_onnx.onnx")
+
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(torch.tensor(state, dtype=torch.float))}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    # check if output matches
+    ort_outs, dqn(state)
+
+    # test on the env using onnx model
+    # We are going to use this block of code for testing your model on the server.
+    # Make sure your model works in this block of code, before submitting.
+    return_ = []
+    for i in range(50):
+        state = env.reset()
+        ret = 0
+        done = False
+        while not done:
+            ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(torch.tensor(state, dtype=torch.float))}
+            q_actions = ort_session.run(None, ort_inputs)
+            action = np.argmax(q_actions)
+            state, r, done, _ = env.step(action)
+            ret += r
+        return_.append(ret)
+
+    print("Average Return:", np.mean(return_))
+
+
+main()
---
+++ config.py
@@ -0,0 +1,9 @@
+import experiment_buddy
+
+experiment_buddy.register(locals())
+tensorboard = experiment_buddy.deploy(
+    "mila",
+    sweep_yaml="",
+    proc_num=1,
+    wandb_kwargs={"entity": "ionelia"}
+)
