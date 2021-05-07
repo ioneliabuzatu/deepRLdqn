5234b5eb404c702945eeb1a318a8e3d6a8908782
ioneliabuzatu <buzatuionelia@gmail.com>
2021-05-07 16:54:56 +0200

    cluster

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import experiment_buddy
 
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
-    "",
+    "mila",
     sweep_yaml="",
     proc_num=1,
     wandb_kwargs={"entity": "ionelia"}
